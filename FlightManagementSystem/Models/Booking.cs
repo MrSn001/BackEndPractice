1e05a147c60d0ab869b41e9a20ed5a8109dc17f8
MrSn001 <shaheen36.saa@gmail.com>
2026-06-23 17:11:42 +0400

    Implemented Booking Class

---
+++ FlightManagementSystem/Models/Booking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightManagementSystem.Models
+{
+    public class Booking
+    {
+        public int bookingId { get; set; } // Auto Generated 
+        public int passengerId { get; set; } // From Passenger List
+        public int flightId { get; set; } // From Flight List
+        public string seatNumber { get; set; } // Calculated From Flight
+        public string bookingDate { get; set; } // Default Value
+        public decimal totalPrice { get; set; } // Calculated From Flight
+        public string status { get; set; } // Default Value
+    }
+}
