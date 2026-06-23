a220895cfe35d8ce271531bc3cb641150b92cba6
MrSn001 <shaheen36.saa@gmail.com>
2026-06-23 17:33:36 +0400

    initialized FlightManagementSystemContext with default data collections

--- FlightManagementSystem/Program.cs
+++ FlightManagementSystem/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace FlightManagementSystem
+﻿using FlightManagementSystem.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FlightManagementSystem
 {
     internal class Program
     {
+        public static FlightManagementSystemContext context = new FlightManagementSystemContext
+        {
+            Aircrafts = new List<Aircraft>(),
+            Bookings = new List<Booking>(),
+            Flights = new List<Flight>(),
+            Passengers = new List<Passenger>(),
+            Pilots = new List<Pilot>()
+        };
         static void Main(string[] args)
         {
-
+           
         }
     }
 }
