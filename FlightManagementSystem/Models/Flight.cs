fcec41a5581060e41b960872dd4fd6a46802dda3
MrSn001 <shaheen36.saa@gmail.com>
2026-06-23 16:38:35 +0400

    Implemented Flight Class

---
+++ FlightManagementSystem/Models/Flight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightManagementSystem.Models
+{
+    internal class Flight
+    {
+        public int flightId {  get; set; } // System Generated 
+        public string flightCode { get; set; } // System Generated 
+        public int aircraftId { get; set; } // From Aircraft List
+        public int pilotId { get; set; } // From Pilot List
+        public string origin {  get; set; } // User Input
+        public string destination { get; set; } // User Input
+        public string departureDate { get; set; } // User Input
+        public string departureTime { get; set; } // User Input
+        public decimal ticketPrice { get; set; } // Calculated
+        public int availableSeats { get; set; } // Calculated
+        public string status { get; set; } // Default Value
+    }
+}
