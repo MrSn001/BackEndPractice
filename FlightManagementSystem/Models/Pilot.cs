2c68a391653de0747150fd77be5b11df0ecec64a
MrSn001 <shaheen36.saa@gmail.com>
2026-06-23 15:49:42 +0400

    Implemented Pilot Class

---
+++ FlightManagementSystem/Models/Pilot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightManagementSystem.Models
+{
+    internal class Pilot
+    {
+        public int pilotId { get; set; } // System Generated 
+        public string pilotName { get; set; } // User Input
+        public string pilotPhone { get; set; } // User Input
+        public string licenseNumber { get; set; } // User Input 
+        public int flightHours { get; set; } // User Input
+        public bool isAvailable { get; set; } // Default Value
+    }
+}
