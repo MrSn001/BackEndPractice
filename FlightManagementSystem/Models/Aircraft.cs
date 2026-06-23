0b50d5b863257216bf4996d7178d6d9267fbf841
MrSn001 <shaheen36.saa@gmail.com>
2026-06-23 15:58:09 +0400

    Implemented Aircraft Class

---
+++ FlightManagementSystem/Models/Aircraft.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightManagementSystem.Models
+{
+    internal class Aircraft
+    {
+        public int aircraftId {  get; set; } // System Generated
+        public string model {  get; set; } // User Input
+        public int totalSeats { get; set; } // User Input
+        public bool isOperational { get; set; } // Default Value
+    }
+}
