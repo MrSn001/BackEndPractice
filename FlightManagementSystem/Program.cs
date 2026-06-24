05044750dd427083d2eadb23ab2f76a7cceb983a
MrSn001 <shaheen36.saa@gmail.com>
2026-06-24 08:56:45 +0400

    Implemented PrintMainMenu() Method

--- FlightManagementSystem/Program.cs
+++ FlightManagementSystem/Program.cs
@@ -5,6 +5,9 @@ namespace FlightManagementSystem
 {
     internal class Program
     {
+        public static bool flag = true;
+        public static int choice;
+
         public static FlightManagementSystemContext context = new FlightManagementSystemContext
         {
             Aircrafts = new List<Aircraft>(),
@@ -13,9 +16,38 @@ namespace FlightManagementSystem
             Passengers = new List<Passenger>(),
             Pilots = new List<Pilot>()
         };
+
+        public static void PrintMainMenu()
+        {
+            Console.WriteLine("""
+                =============================================================
+                            Welcome To Our Flight Managment System
+                =============================================================
+                1. Register a Passenger
+                2. Add an Aircraft
+                3. Register a Pilot
+                4. View All Flights
+                5. Schedule a Flight
+                6. Book a Flight
+                7. Cancel a Booking 
+                8. Depart a Flight
+                9. Cancel a Flight
+                10. Passenger Booking History
+                11. Flight Revenue & Load Factor Report
+                =============================================================
+                Choose a Number: 
+                """);
+        }
+
+
         static void Main(string[] args)
         {
-           
+            while (flag) 
+            {
+                PrintMainMenu();
+              
+
+            }
         }
     }
 }
