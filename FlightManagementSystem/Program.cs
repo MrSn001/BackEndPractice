981e6dac5072927b9f1efd472e7ab907f74ea66e
MrSn001 <shaheen36.saa@gmail.com>
2026-06-29 17:15:53 +0400

    Refactor: CheckIfAircraftIsAvailable(int num) Method assign aircraftId

--- FlightManagementSystem/Program.cs
+++ FlightManagementSystem/Program.cs
@@ -431,6 +431,7 @@ namespace FlightManagementSystem
                 Console.ResetColor();
                 validationFlag = false;
             }
+            aircraftId = num;
         }
 
         public static void PrintAvailablePilots()
