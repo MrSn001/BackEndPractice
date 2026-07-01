d49303d6463002a5f60134c5f587d96a43f76cb6
MrSn001 <shaheen36.saa@gmail.com>
2026-07-01 11:51:15 +0400

    Implemented Enrollment Constraints

--- UniversitySystem/Models/Enrollment.cs
+++ UniversitySystem/Models/Enrollment.cs
@@ -1,14 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace UniversitySystem.Models
 {
     public class Enrollment
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [NotNull]
         public int enrollmentId; // System Generated 
+
+        [Required]
         public DateTime enrollmentDate; //Calculated
+
+        [MaybeNull]
+        [MaxLength(2)]
         public string finalGrade; // Default Value/ Calculated
+
+        [Required]
+        [MaxLength(20)]
+        [DefaultValue("In Progress")]
         public string status; // Default Value/ Calculated
     }
 }
