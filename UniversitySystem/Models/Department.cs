2395a7d3e4c74d5d1c8f514422ce5b1a806f05c0
MrSn001 <shaheen36.saa@gmail.com>
2026-07-01 10:56:05 +0400

    Implemented Department.cs Class

---
+++ UniversitySystem/Models/Department.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace UniversitySystem.Models
+{
+    [Index(nameof(departmentName), IsUnique = true)]
+    public class Department
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [NotNull]
+        public int departmentId {  get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string departmentName { get; set; }
+
+        [MaybeNull]
+        [MaxLength(50)]
+        public string building {  get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue)]
+        public decimal budget { get; set; }
+
+    }
+}
