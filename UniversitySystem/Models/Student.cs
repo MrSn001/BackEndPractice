bda9fec3538949d9e7eb5d2c91a8ae31802a46be
MrSn001 <shaheen36.saa@gmail.com>
2026-07-01 11:26:56 +0400

    Added Coments

--- UniversitySystem/Models/Student.cs
+++ UniversitySystem/Models/Student.cs
@@ -16,29 +16,29 @@ namespace UniversitySystem.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [NotNull]
-        public int studentId {  get; set; }
+        public int studentId {  get; set; } // System Generated
 
         [Required]
         [MaxLength(100)]
-        public string fullName { get; set; }
+        public string fullName { get; set; } // User Input
 
         [Required]
         [MaxLength (150)]
-        public string email { get; set; }
+        public string email { get; set; } // User Input
 
         [AllowNull]
         [MaxLength(20)]
-        public string phoneNumber { get; set; }
+        public string phoneNumber { get; set; } // User Input
 
         [Required]
-        public DateTime dateOfBirth { get; set; }
+        public DateTime dateOfBirth { get; set; } // User Input
 
         [Required]
         [Range(2000,2030)]
-        public int enrollmentYear { get; set; }
+        public int enrollmentYear { get; set; } // Calculated
 
         [DefaultValue(0.0)]
         [Range(0.0,4.0)]
-        public decimal gpa { get; set; }
+        public decimal gpa { get; set; } // DefaultValue / Calculated
     }
 }
