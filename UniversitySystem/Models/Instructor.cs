550d6aa337cd902b525f245043c8899f86c067ae
MrSn001 <shaheen36.saa@gmail.com>
2026-07-01 10:40:03 +0400

    Implemented Instructor.cs Class

---
+++ UniversitySystem/Models/Instructor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace UniversitySystem.Models
+{
+    [Index(nameof(email), IsUnique = true)]
+    public class Instructor
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [NotNull]
+        public int instructorId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string fullName { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string email { get; set; }
+
+        [MaybeNull]
+        [MaxLength(20)]
+        public string officeNumber { get; set; }
+
+        [Required]
+        public DateTime hireDate { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue)]
+        public decimal salary { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [EnumDataType(typeof(AcademicTitle))]
+        public AcademicTitle academicTitle { get; set; }
+
+    }
+}
