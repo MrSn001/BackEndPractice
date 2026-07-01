72bae85d79ec061feae75fc9c40a1bf1d2333823
MrSn001 <shaheen36.saa@gmail.com>
2026-07-01 11:45:09 +0400

    Implemented Enrollment.cs Class

---
+++ UniversitySystem/Models/Enrollment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversitySystem.Models
+{
+    public class Enrollment
+    {
+        public int enrollmentId;
+        public DateTime enrollmentDate;
+        public string finalGrade;
+        public string status;
+    }
+}
