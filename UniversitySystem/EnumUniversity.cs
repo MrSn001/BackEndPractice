b7ddb6115fc60e8b43d12bcfa6919808096822d4
MrSn001 <shaheen36.saa@gmail.com>
2026-07-01 10:39:36 +0400

    Implemented EnumUniversity.cs Class

---
+++ UniversitySystem/EnumUniversity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversitySystem
+{
+    public enum AcademicTitle
+    {
+        Professor,
+        Lecturer
+    }
+
+    public class EnumUniversity
+    {
+    }
+}
