a180ca803172c4211eac36734896a2a1782b8db7
MrSn001 <shaheen36.saa@gmail.com>
2026-07-06 08:27:28 +0400

    Implemented User Class

--- E-CommerceSystem/Models/User.cs
+++ E-CommerceSystem/Models/User.cs
@@ -6,5 +6,14 @@ namespace E_CommerceSystem.Models
 {
     public class User
     {
+        public int userId { get; set; } // System Generated
+        public string username { get; set; } // User Input
+        public string email { get; set; }// User Input
+        public string passwordHash { get; set; }// System Calculated
+        public string fullName { get; set; } // User Input
+        public string phoneNumber { get; set; } // User Input
+        public string address { get; set; } // User Input
+        public DateTime registartionDate { get; set; } // System Calculated
+        public bool isActive { get; set; } // Default Value
     }
 }
