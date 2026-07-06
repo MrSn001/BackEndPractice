7818af7e0c6886c3e2610360b4fd640c94856976
MrSn001 <shaheen36.saa@gmail.com>
2026-07-06 14:50:24 +0400

    Implemented Constraints to the Product.cs Class

--- E-CommerceSystem/Models/Product.cs
+++ E-CommerceSystem/Models/Product.cs
@@ -1,18 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_CommerceSystem.Models
 {
     public class Product
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int productId { get; set; }
+
+        [Required]
+        [MaxLength(150)]
         public string productName { get; set; }
-        public string description { get; set; }
+
+        [MaxLength(1000)]
+        public string? description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue)]
         public decimal price { get; set; }
-        public int stockQuantity { get; set; }
-        public string imageUrl { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int stockQuantity { get; set; } = 0;
+
+        [MaxLength(300)]
+        public string? imageUrl { get; set; }
+
+        [Required]
         public DateTime createdDate { get; set; } = DateTime.Now;
+
         public bool isAvailable { get; set; } = true;
     }
 }
