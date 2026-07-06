80fedc7c82c31937f3bfdd9d3fb07e530cd5a1cb
MrSn001 <shaheen36.saa@gmail.com>
2026-07-06 14:56:41 +0400

    Implemented Order.cs Class properties

--- E-CommerceSystem/Models/Order.cs
+++ E-CommerceSystem/Models/Order.cs
@@ -6,6 +6,11 @@ namespace E_CommerceSystem.Models
 {
     public class Order
     {
-        
+        public int orderId {  get; set; } // System Generated
+        public DateTime orderDate { get; set; } = DateTime.Now // System Calculated
+        public decimal totalAmount { get; set; } // System Calculated
+        public string status { get; set; } = "Pending" // default value
+        public string shippingAddress { get; set; } // User Input
+        public string paymentMethod { get; set; } // User Input
     }
 }
