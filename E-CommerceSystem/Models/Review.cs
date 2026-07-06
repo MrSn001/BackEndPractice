9638e3ed9120467fe88f1d339d5f79d926e0fb8f
MrSn001 <shaheen36.saa@gmail.com>
2026-07-06 15:12:06 +0400

    Implemented Review.cs Class Constraints

--- E-CommerceSystem/Models/Review.cs
+++ E-CommerceSystem/Models/Review.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_CommerceSystem.Models
 {
     public class Review
     {
-        public int reviewId { get; set; }
-        public int rating { get; set; }
-        public string comment { get; set; }
-        public DateTime reviewDate { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int reviewId { get; set; }// System Generated 
+
+        [Required]
+        [Range(1,5)]
+        public int rating { get; set; } // User Input
+
+        [MaxLength(1000)]
+        public string? comment { get; set; } // User Input
+
+        [Required]
+        public DateTime reviewDate { get; set; } = DateTime.Now; // Default value
     }
 }
