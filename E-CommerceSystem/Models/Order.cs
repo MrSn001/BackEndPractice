dda4c4a2127501fd626ff1f4bb43fa1b7ce10d8a
MrSn001 <shaheen36.saa@gmail.com>
2026-07-06 15:07:12 +0400

    Implemented Order.cs Class Constraints

--- E-CommerceSystem/Models/Order.cs
+++ E-CommerceSystem/Models/Order.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_CommerceSystem.Models
 {
     public class Order
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int orderId {  get; set; } // System Generated
-        public DateTime orderDate { get; set; } = DateTime.Now // System Calculated
+
+        [Required]
+        public DateTime orderDate { get; set; } = DateTime.Now; // System Calculated
+
+        [Required]
+        [Range(0.0, decimal.MaxValue)]
         public decimal totalAmount { get; set; } // System Calculated
-        public string status { get; set; } = "Pending" // default value
+
+        [Required]
+        [MaxLength(30)]
+        public string status { get; set; } = "Pending"; // default value
+
+        [Required]
+        [MaxLength(300)]
         public string shippingAddress { get; set; } // User Input
+
+        [Required]
+        [MaxLength(50)]
         public string paymentMethod { get; set; } // User Input
     }
 }
