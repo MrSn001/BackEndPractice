f6c9fe6307cf5df4b594ddf25b9b76f51232ffa1
MrSn001 <shaheen36.saa@gmail.com>
2026-07-06 15:09:23 +0400

    Implemented Review.cs Class properties

--- E-CommerceSystem/Models/Review.cs
+++ E-CommerceSystem/Models/Review.cs
@@ -6,5 +6,9 @@ namespace E_CommerceSystem.Models
 {
     public class Review
     {
+        public int reviewId { get; set; }
+        public int rating { get; set; }
+        public string comment { get; set; }
+        public DateTime reviewDate { get; set; }
     }
 }
