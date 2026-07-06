554e35b57d5eab2cb3df64dba699744094c9e37f
MrSn001 <shaheen36.saa@gmail.com>
2026-07-06 16:59:09 +0400

    Implemented OrderItem.cs Class

--- E-CommerceSystem/Models/OrderItem.cs
+++ E-CommerceSystem/Models/OrderItem.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace E_CommerceSystem.Models
 {
     public class OrderItem
     {
+        public int orderItemId { get; set; } // System Generated
+        public int quantity { get; set; } // User Input
     }
 }
