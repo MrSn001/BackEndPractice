eb38602070b2e4849a9eebc93c482166bf7d068e
MrSn001 <shaheen36.saa@gmail.com>
2026-07-06 14:25:05 +0400

    Implemented Constraints to the Category.cs Class Properties

--- E-CommerceSystem/Models/Category.cs
+++ E-CommerceSystem/Models/Category.cs
@@ -1,14 +1,26 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_CommerceSystem.Models
 {
+    [Index(nameof(categoryName), IsUnique = true)]
     public class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int categoryId { get; set; } // System Generated
+
+        [Required]
+        [MaxLength(100)]
         public string categoryName { get; set; } // User Input
-        public string description { get; set; } // User Input
-        public string imageUrl { get; set; } // User Input
+
+        [MaxLength(500)]
+        public string? description { get; set; } // User Input
+        [MaxLength(300)]
+        public string? imageUrl { get; set; } // User Input
     }
 }
