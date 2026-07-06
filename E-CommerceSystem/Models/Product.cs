f08dea98700e025fc4ab96d2744e3141ae9996b9
MrSn001 <shaheen36.saa@gmail.com>
2026-07-06 14:34:12 +0400

    Implemented properties to the Product.cs Class

--- E-CommerceSystem/Models/Product.cs
+++ E-CommerceSystem/Models/Product.cs
@@ -6,5 +6,13 @@ namespace E_CommerceSystem.Models
 {
     public class Product
     {
+        public int productId { get; set; }
+        public string productName { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public int stockQuantity { get; set; }
+        public string imageUrl { get; set; }
+        public DateTime createdDate { get; set; } = DateTime.Now;
+        public bool isAvailable { get; set; } = true;
     }
 }
