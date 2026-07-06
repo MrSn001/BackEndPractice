287d36c6304a4deff1e08b7cd5ac716952e1a158
MrSn001 <shaheen36.saa@gmail.com>
2026-07-06 08:42:39 +0400

    Implemented Category Class Properties

--- E-CommerceSystem/Models/Category.cs
+++ E-CommerceSystem/Models/Category.cs
@@ -6,5 +6,9 @@ namespace E_CommerceSystem.Models
 {
     public class Category
     {
+        public int categoryId { get; set; } // System Generated
+        public string categoryName { get; set; } // User Input
+        public string description { get; set; } // User Input
+        public string imageUrl { get; set; } // User Input
     }
 }
