3c1ccab2c90e2a7a3e9fc7f00ad1d2d1df91e814
MrSn001 <shaheen36.saa@gmail.com>
2026-07-06 08:35:36 +0400

    Implementeed the Constraints in user Class

--- E-CommerceSystem/Models/User.cs
+++ E-CommerceSystem/Models/User.cs
@@ -1,19 +1,44 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_CommerceSystem.Models
 {
+    [Index(nameof(username), IsUnique = true)]
+    [Index(nameof(email), IsUnique = true)]
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int userId { get; set; } // System Generated
+
+        [Required]
+        [MaxLength(50)]
         public string username { get; set; } // User Input
+
+        [Required]
+        [MaxLength(150)]
         public string email { get; set; }// User Input
+
+        [Required]
+        [MaxLength(256)]
         public string passwordHash { get; set; }// System Calculated
+
+        [Required]
+        [MaxLength (100)]
         public string fullName { get; set; } // User Input
-        public string phoneNumber { get; set; } // User Input
-        public string address { get; set; } // User Input
+
+        [MaxLength(20)]
+        public string? phoneNumber { get; set; } // User Input
+
+        [MaxLength(300)]
+        public string? address { get; set; } // User Input
+
+        [Required]
         public DateTime registartionDate { get; set; } // System Calculated
-        public bool isActive { get; set; } // Default Value
+        public bool isActive { get; set; } = true; // Default Value
     }
 }
