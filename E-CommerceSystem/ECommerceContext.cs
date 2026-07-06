2968a25b8ce259c8948146d62f98c2145bedea4a
MrSn001 <shaheen36.saa@gmail.com>
2026-07-06 19:02:11 +0400

    Implemented the DbSet collection for all the classes

--- E-CommerceSystem/ECommerceContext.cs
+++ E-CommerceSystem/ECommerceContext.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using E_CommerceSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace E_CommerceSystem
 {
-    public class ECommerceContext
+    public class ECommerceContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
     }
 }
